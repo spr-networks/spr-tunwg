b70f7c2be7d4ba66982ec7c999aca000ec6fcad4
Alex Rad <alex@supernetworks.org>
2026-07-20 19:02:49 -0700

    Isolate krun plugin networking

--- install.sh
+++ install.sh
@@ -30,12 +30,12 @@ if [ ! -f "$SUPERDIR/configs/plugins/spr-tunwg/config.json" ]; then
 fi
 
 KRUN_MAC="02:53:50:52:4b:11"
-KRUN_TAP="ktunwg0"
+PLUGIN_INTERFACE="spr-tunwg"
 curl --fail-with-body --silent --show-error "http://127.0.0.1/device?identity=${KRUN_MAC}" \
   -H "Authorization: Bearer ${SPR_API_TOKEN}" -H "Content-Type: application/json" \
   -X PUT --data-raw "{\"MAC\":\"${KRUN_MAC}\",\"Name\":\"spr-tunwg\",\"Policies\":[\"wan\"],\"Groups\":[\"tunwg\"]}" >/dev/null
-if ! sudo nft get element inet filter dhcp_access "{ \"${KRUN_TAP}\" . ${KRUN_MAC} }" >/dev/null 2>&1; then
-  sudo nft add element inet filter dhcp_access "{ \"${KRUN_TAP}\" . ${KRUN_MAC} : accept }"
+if ! sudo nft get element inet filter dhcp_access "{ \"${PLUGIN_INTERFACE}\" . ${KRUN_MAC} }" >/dev/null 2>&1; then
+  sudo nft add element inet filter dhcp_access "{ \"${PLUGIN_INTERFACE}\" . ${KRUN_MAC} : accept }"
 fi
 
 docker compose -f docker-compose-kvm.yml build
@@ -52,6 +52,6 @@ API=127.0.0.1
 curl "http://${API}/firewall/custom_interface" \
 -H "Authorization: Bearer ${SPR_API_TOKEN}" \
 -X 'PUT' \
---data-raw "{\"SrcIP\":\"${CONTAINER_IP}\",\"Interface\":\"${KRUN_TAP}\",\"Policies\":[\"wan\"],\"Groups\":[\"tunwg\"]}"
+--data-raw "{\"SrcIP\":\"${CONTAINER_IP}\",\"Interface\":\"${PLUGIN_INTERFACE}\",\"Policies\":[\"wan\"],\"Groups\":[\"tunwg\"]}"
 
 docker compose -f docker-compose-kvm.yml restart
